affd6f020863dff329f11626d6583933773c6f15
Em Lazer-Walker <me@lazerwalker.com>
2020-08-21 18:18:10 -0400

    Try to remove dev tools

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      devTools: false
+    }
   });
 
   // and load the index.html of the app.
